fce9b30f865172d17ae168fe3bc74188b666ef9f
Umer Naseer <umernaseer@Umers-MacBook-Pro.local>
2024-05-29 09:49:21 +0100

    provides an object of all available endpoints and their description

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getTopics, invalidRoutes } = require("./controllers/topics.controllers");
+const { getTopics, invalidRoutes, getEndpoints } = require("./controllers/topics.controllers");
 const { handleCustomErrors, handleServerErrors } = require("./error-handlers/error-handler");
 
 const app = express();
 
 app.get('/api/topics',getTopics)
+app.get('/api',getEndpoints)
+
 app.get('*',invalidRoutes)
 
 app.use(handleCustomErrors)
