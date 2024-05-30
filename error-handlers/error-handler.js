3cd1fd7313565c2ede29e8a9f4d6989fbe8a1284
Umer Naseer <umernaseer@Umers-MacBook-Pro.local>
2024-05-30 17:06:25 +0100

    hosting...

--- error-handlers/error-handler.js
+++ error-handlers/error-handler.js
@@ -9,8 +9,8 @@ exports.handlePsqlErrors=(err,req,res,next)=>{
     }
     else if(err.code==='23503') {
         res.status(404).send({ msg: 'Not Found' })
-    }
-    next(err)
+    }else
+        next(err)
 }
 exports.handleServerErrors = (err, req, res, next) => {
     res.status(500).send({ msg: 'Internal Server Error' });
