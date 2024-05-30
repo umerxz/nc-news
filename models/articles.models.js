e0e03eebddaec06711b4f04ecf3deed587e54cc4
Umer Naseer <umernaseer@Umers-MacBook-Pro.local>
2024-05-30 14:49:14 +0100

    get article by id with comment count for that article

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,10 @@ const db = require('../db/connection')
 const format = require('pg-format')
 exports.fetchArticleById = (id) =>{
     return db.query(
-        `SELECT * FROM articles WHERE article_id = $1;`,[id]
+        `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles 
+        LEFT JOIN comments ON comments.article_id = articles.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id`,[id]
     )
     .then((result)=>{
         if(result.rows.length===0){
